1b2914770a86760a4a33d7e5e9cd2f926583c58d
Alexander <alexanderl96@yahoo.com>
2023-04-04 16:50:46 -0400

    asdkh

--- db/seed.sql
+++ db/seed.sql
@@ -1,9 +1,9 @@
 -- \c usersbackend;
 INSERT INTO useraccount (username, email, image, password) VALUES 
-('Robert La Rosa', 'alexayarini@gmail.com' , null, '12345678');
+('Albert2 La Rosa', 'alexayarinil4@gmail.com' , '', '12345678');
 
 INSERT INTO administer (username, email, image, password) VALUES 
-('Alexander La Rosa', 'alexanderl96@yahoo.com' , null, '87654321');
+('Alexander3 La Rosa', 'alexanderl9@yahoo.com' , '', '87654321');
 
 INSERT INTO comments (username, userimage, comment, memberid, date) VALUES 
-(null, null, null, null, null);
+('', '', '', '', '');
