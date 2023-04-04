cfac9e2bd335ea76fe5a29c45e189871d8724539
Alexander <alexanderl96@yahoo.com>
2023-04-04 14:02:46 -0400

    fixing bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.NODE_ENV;
 
 app.listen(PORT, () => {
   console.log(`🪨 Listening on port ${PORT} 💎 `);
